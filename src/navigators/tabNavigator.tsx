3c530b2f1d1fdf7b77d7a29bcd5cc35d48301dd9
Daniel Neziri <freelance.daniel@outlook.com>
2021-07-24 00:38:19 -0500

    Customized Bottom Tabbar

--- src/navigators/tabNavigator.tsx
+++ src/navigators/tabNavigator.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View } from 'react-native';
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+  BottomTabBarProps,
+  BottomTabBarOptions,
+} from '@react-navigation/bottom-tabs';
 import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
 
 import { Home, Info } from '../screens';
 import Constants from '../const';
 
 const Tab = createBottomTabNavigator();
+
+function TabBar(props: BottomTabBarProps<BottomTabBarOptions>): JSX.Element {
+  const { state, descriptors, navigation } = props;
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        left: 20,
+        right: 20,
+        bottom: 30,
+        borderRadius: 20,
+        shadowColor: 'black',
+        shadowOffset: {
+          width: 10,
+          height: 10,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 10,
+        elevation: 15,
+      }}
+    >
+      <BottomTabBar
+        state={state}
+        descriptors={descriptors}
+        navigation={navigation}
+        showLabel={false}
+        style={{
+          borderRadius: 20,
+        }}
+        safeAreaInsets={{
+          bottom: 10,
+        }}
+      />
+    </View>
+  );
+}
 
 export default function TabNavigator(): JSX.Element {
   return (
@@ -39,10 +81,13 @@ export default function TabNavigator(): JSX.Element {
           );
         },
       })}
-      tabBarOptions={{
-        style: {},
-        showLabel: false,
-      }}
+      tabBar={props => (
+        <TabBar
+          state={props.state}
+          descriptors={props.descriptors}
+          navigation={props.navigation}
+        />
+      )}
     >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Info" component={Info} />
